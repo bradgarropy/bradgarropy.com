1ada941386d2819e1effb1b9bebd94f7cbf7f8e4
Brad Garropy <bradgarropy@gmail.com>
2018-08-13 19:48:35 -0500

    removing unnecessary emoji parser.

--- gatsby-config.js
+++ gatsby-config.js
@@ -49,11 +49,6 @@ module.exports = {
         },
         {
             resolve: "gatsby-transformer-remark",
-            options: {
-                plugins: [
-                    "gatsby-remark-emoji",
-                ],
-            },
         },
     ],
 }
