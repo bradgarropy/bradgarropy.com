39346539e0c9c2a1e20c3dc0ac5ed5b6df06aa90
Brad Garropy <bradgarropy@gmail.com>
2019-04-27 13:44:34 -0500

    post meta styles.

--- src/components/PostMeta.js
+++ src/components/PostMeta.js
@@ -1,15 +1,29 @@
 import React from "react"
 import {Link} from "gatsby"
 import PropTypes from "prop-types"
-import "../scss/PostMeta.scss"
+import styled from "styled-components"
+
+const PostMetaWrapper = styled.div`
+    p {
+        margin: 0rem;
+    }
+
+    a {
+        color: rgba($color: black, $alpha: 0.8);
+
+        &:hover {
+            color: ${({theme}) => theme.colors.blue};
+        }
+    }
+`
 
 const PostMeta = ({date, topic}) => {
     return (
-        <div className="post-meta">
+        <PostMetaWrapper>
             <p>
                 {date} in <Link to={`/topic/${topic}`}>#{topic}</Link>
             </p>
-        </div>
+        </PostMetaWrapper>
     )
 }
 
