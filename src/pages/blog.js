1de67bf090b65404a795e3bf659bf501aea8199e
Brad Garropy <bradgarropy@gmail.com>
2019-05-21 22:56:27 -0500

    blog page. closes #55.

---
+++ src/pages/blog.js
@@ -0,0 +1,49 @@
+import React from "react"
+import PropTypes from "prop-types"
+import {graphql} from "gatsby"
+import Layout from "../components/Layout"
+import Meta from "../components/SEO/Meta"
+import Facebook from "../components/SEO/Facebook"
+import Twitter from "../components/SEO/Twitter"
+import PostList from "../components/PostList"
+
+const blogPage = ({data}) => {
+    const posts = data.allMarkdownRemark.edges.map(edge => edge.node)
+
+    return (
+        <Layout>
+            <Meta title="blog"/>
+            <Facebook/>
+            <Twitter/>
+
+            <PostList posts={posts}/>
+        </Layout>
+    )
+}
+
+export const blogPageQuery = graphql`
+    {
+        allMarkdownRemark(sort: {fields: frontmatter___date, order: DESC}) {
+            edges {
+                node {
+                    frontmatter {
+                        slug
+                        title
+                        date(formatString: "MMMM D, YYYY")
+                        topic {
+                            name
+                            icon
+                        }
+                    }
+                }
+            }
+        }
+    }
+`
+
+blogPage.propTypes = {
+    data: PropTypes.object.isRequired,
+    location: PropTypes.object,
+}
+
+export default blogPage
