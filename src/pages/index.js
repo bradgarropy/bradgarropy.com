b16aa480aa6fce7b2082fc4dc3c23f521d17e9ee
Brad Garropy <bradgarropy@gmail.com>
2019-05-21 23:26:06 -0500

    temporary index page.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,49 +1,18 @@
 import React from "react"
-import PropTypes from "prop-types"
-import {graphql} from "gatsby"
 import Layout from "../components/Layout"
 import Meta from "../components/SEO/Meta"
 import Facebook from "../components/SEO/Facebook"
 import Twitter from "../components/SEO/Twitter"
-import PostList from "../components/PostList"
 
-const blogPage = ({data}) => {
-    const posts = data.allMarkdownRemark.edges.map(edge => edge.node)
-
+const indexPage = () => {
     return (
         <Layout>
             <Meta title="blog"/>
             <Facebook/>
             <Twitter/>
-
-            <PostList posts={posts}/>
+            <p>suh dude.</p>
         </Layout>
     )
 }
 
-export const blogPageQuery = graphql`
-    {
-        allMarkdownRemark(sort: {fields: frontmatter___date, order: DESC}) {
-            edges {
-                node {
-                    frontmatter {
-                        slug
-                        title
-                        date(formatString: "MMMM D, YYYY")
-                        topic {
-                            name
-                            icon
-                        }
-                    }
-                }
-            }
-        }
-    }
-`
-
-blogPage.propTypes = {
-    data: PropTypes.object.isRequired,
-    location: PropTypes.object,
-}
-
-export default blogPage
+export default indexPage
