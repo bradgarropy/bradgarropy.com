0a5e8c42c8fbbf32cf30fd7f3d5e06a4464cbb64
Brad Garropy <bradgarropy@gmail.com>
2019-05-23 22:28:40 -0500

    display uses markdown.

--- src/pages/uses.js
+++ src/pages/uses.js
@@ -1,19 +1,41 @@
 import React from "react"
+import PropTypes from "prop-types"
+import {graphql} from "gatsby"
 import Layout from "../components/Layout"
 import Meta from "../components/SEO/Meta"
 import Facebook from "../components/SEO/Facebook"
 import Twitter from "../components/SEO/Twitter"
 
-const UsesPage = () => {
+const UsesPage = ({data}) => {
+    const {html} = data.uses
+
     return (
         <Layout>
             <Meta title="uses"/>
             <Facebook/>
             <Twitter/>
 
-            <p>coming soon.</p>
+            <div
+                dangerouslySetInnerHTML={{
+                    __html: html,
+                }}
+            />
         </Layout>
     )
 }
+
+UsesPage.propTypes = {
+    data: PropTypes.object.isRequired,
+}
+
+export const usesPageQuery = graphql`
+    {
+        uses: markdownRemark(
+            fileAbsolutePath: {regex: "/content/uses/index.md/"}
+        ) {
+            html
+        }
+    }
+`
 
 export default UsesPage
