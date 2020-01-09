d3de3937c9ffbea98d9f6e977fadc663605937f7
Brad Garropy <bradgarropy@gmail.com>
2020-01-08 21:53:01 -0600

    home page social card. closes #91.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,31 @@
 import React from "react"
+import {graphql, useStaticQuery} from "gatsby"
+import Hero from "../components/Hero"
 import Layout from "../components/Layout"
 import Meta from "../components/SEO/Meta"
-import Facebook from "../components/SEO/Facebook"
 import Twitter from "../components/SEO/Twitter"
-import Hero from "../components/Hero"
+import Facebook from "../components/SEO/Facebook"
 
 const IndexPage = () => {
+    const data = useStaticQuery(graphql`
+        {
+            site {
+                siteMetadata {
+                    description
+                }
+            }
+        }
+    `)
+
+    const {description} = data.site.siteMetadata
+
     return (
         <Layout>
-            <Meta title="bradgarropy"/>
-            <Facebook/>
-            <Twitter/>
+            <Meta title="bradgarropy" />
+            <Facebook title={description} description="" />
+            <Twitter title={description} description="" />
 
-            <Hero/>
+            <Hero />
         </Layout>
     )
 }
