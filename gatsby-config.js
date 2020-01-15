c7cb90a21e23c6e1707918d6076839f1db24208a
Brad Garropy <bradgarropy@gmail.com>
2020-01-14 22:04:54 -0600

    remark embedder.

--- gatsby-config.js
+++ gatsby-config.js
@@ -78,6 +78,9 @@ module.exports = {
                     {
                         resolve: "gatsby-remark-prismjs",
                     },
+                    {
+                        resolve: "gatsby-remark-embedder",
+                    },
                 ],
             },
         },
