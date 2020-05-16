0d45a2f4021566b0dee10a031eb47f3a02967c21
Brad Garropy <bradgarropy@gmail.com>
2020-05-15 22:28:49 -0500

    contact page links.

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,20 +1,58 @@
 import React from "react"
+import {graphql} from "gatsby"
+import PropTypes from "prop-types"
+import styled from "styled-components"
 import Layout from "../components/Layout"
 import Meta from "../components/SEO/Meta"
 import Facebook from "../components/SEO/Facebook"
 import Twitter from "../components/SEO/Twitter"
-import Contact from "../components/Contact"
 
-const ContactPage = () => {
+const StyledContact = styled.div`
+    a:not(.anchor) {
+        color: rgba(0, 0, 0, 0.8);
+        box-shadow: inset 0 -2px ${({theme}) => theme.colors.primary};
+
+        &:hover {
+            box-shadow: inset 0 -25px 0 ${({theme}) => theme.colors.primary};
+        }
+    }
+
+    table {
+        font-size: 1.5rem;
+        margin-left: 0;
+    }
+
+    td {
+        padding: 0 1rem 0 0;
+    }
+`
+
+const ContactPage = ({data}) => {
+    const {html} = data.contact
+
     return (
         <Layout>
             <Meta title="contact" />
             <Facebook title="👋🏼 say hi" description="" />
             <Twitter title="👋🏼 say hi" description="" />
 
-            <Contact />
+            <StyledContact dangerouslySetInnerHTML={{__html: html}} />
         </Layout>
     )
 }
+
+ContactPage.propTypes = {
+    data: PropTypes.object.isRequired,
+}
+
+export const query = graphql`
+    {
+        contact: markdownRemark(
+            fileAbsolutePath: {regex: "/content/pages/contact/"}
+        ) {
+            html
+        }
+    }
+`
 
 export default ContactPage
