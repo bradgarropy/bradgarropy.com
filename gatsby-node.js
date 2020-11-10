0030bf4ef186fc83ca3c4347d27fd0f7f0bd5606
Brad Garropy <bradgarropy@gmail.com>
2020-11-10 00:09:09 -0600

    remove annoying react hot loader message.

--- gatsby-node.js
+++ gatsby-node.js
@@ -106,7 +106,15 @@ const createPages = async ({graphql, actions}) => {
     nows.edges.forEach(now => createNow(now, createPage))
 }
 
-const onCreateWebpackConfig = ({actions}) => {
+const onCreateWebpackConfig = ({stage, actions}) => {
+    if (stage.startsWith("develop")) {
+        actions.setWebpackConfig({
+            resolve: {
+                alias: {"react-dom": "@hot-loader/react-dom"},
+            },
+        })
+    }
+
     actions.setWebpackConfig({
         resolve: {
             modules: [path.resolve(__dirname, "src"), "node_modules"],
