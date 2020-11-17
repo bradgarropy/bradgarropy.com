ec0b63295205ddf080646d55c3c70ca84654892c
Brad Garropy <bradgarropy@gmail.com>
2020-11-16 22:59:09 -0600

    better gatsby mock.

--- src/test-utils/setup.js
+++ src/test-utils/setup.js
@@ -1,6 +1,34 @@
 import "jest-styled-components"
 import "@testing-library/jest-dom"
 
-global.___loader = {
-    enqueue: jest.fn(),
-}
+jest.mock("gatsby", () => {
+    const React = require("react")
+    const gatsby = jest.requireActual("gatsby")
+
+    return {
+        ...gatsby,
+        graphql: jest.fn(),
+        Link: jest
+            .fn()
+            .mockImplementation(
+                ({
+                    activeClassName,
+                    activeStyle,
+                    getProps,
+                    innerRef,
+                    partiallyActive,
+                    ref,
+                    replace,
+                    to,
+                    ...rest
+                }) =>
+                    React.createElement("a", {
+                        ...rest,
+                        href: to,
+                        gatsby: "true",
+                    }),
+            ),
+        StaticQuery: jest.fn(),
+        useStaticQuery: jest.fn(),
+    }
+})
