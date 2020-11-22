be50bab5ed5cc57e957dac2e5c31f7eee76a9413
Brad Garropy <bradgarropy@gmail.com>
2020-11-21 22:24:03 -0600

    update jest config.

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,12 @@
 const config = {
+    clearMocks: true,
     collectCoverage: true,
     setupFilesAfterEnv: ["<rootDir>/src/test-utils/setup"],
     moduleDirectories: ["<rootDir>/src", "node_modules"],
+    moduleNameMapper: {
+        "\\.(svg)$": "<rootDir>/src/test-utils/file.js",
+        "\\.(css)$": "identity-obj-proxy",
+    },
     testPathIgnorePatterns: ["<rootDir>/plop"],
 }
 
