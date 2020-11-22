5c6005fa75954c158113d0f0c63bba3b499abf34
Brad Garropy <bradgarropy@gmail.com>
2020-11-21 22:21:45 -0600

    ignore plop.

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ const config = {
     collectCoverage: true,
     setupFilesAfterEnv: ["<rootDir>/src/test-utils/setup"],
     moduleDirectories: ["<rootDir>/src", "node_modules"],
+    testPathIgnorePatterns: ["<rootDir>/plop"],
 }
 
 module.exports = config
