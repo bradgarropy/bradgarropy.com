5759cd9fbf9a088fae29361984e86490b2434e38
Brad Garropy <bradgarropy@gmail.com>
2020-12-07 21:43:29 -0600

    handle youtube timestamps.

--- src/transformers/youtube.js
+++ src/transformers/youtube.js
@@ -1,16 +1,25 @@
 const name = "custom youtube"
 
-const shouldTransform = url => url.includes("youtu.be")
+const shouldTransform = url => {
+    return url.includes("youtu.be")
+}
 
 const getHTML = string => {
-    const url = new URL(string)
-    const id = url.pathname.slice(1)
+    const {pathname, searchParams} = new URL(string)
+    const id = pathname.slice(1)
+    const start = searchParams.get("t")
+
+    let src = `https://www.youtube-nocookie.com/embed/${id}`
+
+    if (start) {
+        src = `${src}?start=${start}`
+    }
 
     const html = `
         <div class="youtube">
             <iframe
                 title=${id}
-                src="https://www.youtube-nocookie.com/embed/${id}"
+                src=${src}
                 frameBorder="0"
                 allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
                 allowFullScreen
