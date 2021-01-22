d51959dd61f4224b7578311cbb7029639bd37bf2
Brad Garropy <bradgarropy@gmail.com>
2021-01-21 22:13:57 -0600

    remove units from line height.

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -34,7 +34,7 @@ const GlobalStyles = createGlobalStyle`
 
     p {
         color: rgba(0, 0, 0, 0.8);
-        line-height: 1.75rem;
+        line-height: 1.75;
         margin-top: 1.75rem;
         margin-bottom: 1.75rem;
     }
