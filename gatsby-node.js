8f4b5f3989ab246d3e462fa63d0ad03a62c200ae
Brad Garropy <bradgarropy@gmail.com>
2021-03-16 21:58:04 -0500

    remove hot loader. remove incremental builds.

--- gatsby-node.js
+++ gatsby-node.js
@@ -106,15 +106,7 @@ const createPages = async ({graphql, actions}) => {
     nows.edges.forEach(now => createNow(now, createPage))
 }
 
-const onCreateWebpackConfig = ({stage, actions}) => {
-    if (stage.startsWith("develop")) {
-        actions.setWebpackConfig({
-            resolve: {
-                alias: {"react-dom": "@hot-loader/react-dom"},
-            },
-        })
-    }
-
+const onCreateWebpackConfig = ({actions}) => {
     actions.setWebpackConfig({
         resolve: {
             modules: [path.resolve(__dirname, "src"), "node_modules"],
