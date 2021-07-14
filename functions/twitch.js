15af9d7077210d65dc594a985dbc9fee96a7c9c4
Brad Garropy <bradgarropy@gmail.com>
2021-07-14 10:50:21 -0500

    try caching the token.

--- functions/twitch.js
+++ functions/twitch.js
@@ -1,18 +1,30 @@
 const http = require("@bradgarropy/http")
 
+let token
+
 const handler = async () => {
     const clientId = process.env.TWITCH_CLIENT_ID
     const clientSecret = process.env.TWITCH_CLIENT_SECRET
 
-    const {access_token} = await http.post(
-        `https://id.twitch.tv/oauth2/token?client_id=${clientId}&client_secret=${clientSecret}&grant_type=client_credentials`,
-    )
+    console.log(`token: ${token}`)
+
+    if (!token) {
+        console.log("no token, authenticating")
+
+        const {access_token} = await http.post(
+            `https://id.twitch.tv/oauth2/token?client_id=${clientId}&client_secret=${clientSecret}&grant_type=client_credentials`,
+        )
+
+        token = access_token
+    }
+
+    console.log(`token: ${token}`)
 
     const channelResponse = await http.get(
         "https://api.twitch.tv/helix/search/channels?query=bradgarropy",
         {
             headers: {
-                "authorization": `Bearer ${access_token}`,
+                "authorization": `Bearer ${token}`,
                 "client-id": clientId,
             },
         },
