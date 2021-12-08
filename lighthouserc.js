e2cef08f36b44946f1a6c2da05ad172ded837c2f
Brad Garropy <bgarropy@atlassian.com>
2021-12-08 13:41:57 -0600

    change assertions to error.

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,7 +1,7 @@
 const config = {
     ci: {
         assert: {
-            "categories:performance": ["warn", {minScore: 0.3}],
+            "categories:performance": ["error", {minScore: 0.3}],
             "categories:accessibility": ["error", {minScore: 0.3}],
             "categories:seo": ["error", {minScore: 0.3}],
             "categories:best-practices": ["error", {minScore: 0.3}],
