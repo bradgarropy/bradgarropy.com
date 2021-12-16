20f454eb584c5d54ada8033cfed64a0f403ffda0
Brad Garropy <bradgarropy@gmail.com>
2021-12-15 23:59:47 -0600

    load svgs as react components.

---
+++ next.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    webpack: config => {
+        config.module.rules.push({
+            test: /\.svg$/,
+            use: [
+                {
+                    loader: "@svgr/webpack",
+                    options: {
+                        svgo: false,
+                    },
+                },
+            ],
+        })
+
+        return config
+    },
+}
