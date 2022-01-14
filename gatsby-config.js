ff2a0c63f973b4db7cb566699dda8078234c7f09
Brad Garropy <bgarropy@atlassian.com>
2022-01-13 22:31:04 -0600

    removing some gatsby files.

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,17 +3,8 @@ require("dotenv").config()
 const twitch = require("./src/transformers/twitch")
 const youtube = require("./src/transformers/youtube")
 
-const youtubeChannelId = "UCgbFhcZKt36Upo7oxWlLEig"
-const captivateRss = "https://feeds.captivate.fm/webdevweekly"
-
 const config = {
-    siteMetadata: {
-        siteUrl: "https://bradgarropy.com",
-    },
     plugins: [
-        {
-            resolve: "gatsby-plugin-react-helmet",
-        },
         {
             resolve: "gatsby-plugin-sitemap",
         },
@@ -31,111 +22,6 @@ const config = {
             },
         },
         {
-            resolve: "gatsby-plugin-web-font-loader",
-            options: {
-                google: {
-                    families: [
-                        "Righteous:400",
-                        "Montserrat:400,500,600,700,800,900",
-                        "Open Sans:400,500,600,700,800,900",
-                    ],
-                },
-            },
-        },
-        {
-            resolve: "gatsby-source-filesystem",
-            options: {
-                name: "posts",
-                path: "content/posts",
-            },
-        },
-        {
-            resolve: "gatsby-source-filesystem",
-            options: {
-                name: "pages",
-                path: "content/pages",
-            },
-        },
-        {
-            resolve: "gatsby-source-filesystem",
-            options: {
-                name: "now",
-                path: "content/now",
-            },
-        },
-        {
-            resolve: "gatsby-source-filesystem",
-            options: {
-                name: "testimonials",
-                path: "content/testimonials",
-            },
-        },
-        {
-            resolve: "gatsby-source-filesystem",
-            options: {
-                name: "images",
-                path: "static",
-            },
-        },
-        {
-            resolve: "gatsby-source-youtube-v3",
-            options: {
-                channelId: [youtubeChannelId],
-                apiKey: process.env.YOUTUBE_API_KEY,
-            },
-        },
-        {
-            resolve: "gatsby-source-anchor",
-            options: {
-                rss: captivateRss,
-            },
-        },
-        {
-            resolve: "gatsby-source-github-api",
-            options: {
-                token: process.env.GITHUB_TOKEN,
-                graphQLQuery: `
-                    query {
-                        user(login: "bradgarropy") {
-                            pinnedItems(types: REPOSITORY, first: 6) {
-                                nodes {
-                                    ... on Repository {
-                                        url
-                                        name
-                                        description
-                                        stargazerCount
-                                        repositoryTopics(first: 20) {
-                                            nodes {
-                                                topic {
-                                                name
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                            sponsorshipsAsMaintainer(first: 10) {
-                                nodes {
-                                    sponsorEntity {
-                                        ... on User {
-                                            login
-                                            url
-                                            avatarUrl
-                                        }
-                                    }
-                                    tier {
-                                        name
-                                        description
-                                        isOneTime
-                                    }
-                                }
-                            }
-                        }
-                    }
-                `,
-            },
-        },
-        {
             resolve: "gatsby-transformer-remark",
             options: {
                 plugins: [
@@ -150,13 +36,6 @@ const config = {
                         options: {
                             maintainCase: false,
                             removeAccents: true,
-                        },
-                    },
-                    {
-                        resolve: "gatsby-remark-vscode",
-                        options: {
-                            theme: "Shades of Purple",
-                            extensions: ["shades-of-purple"],
                         },
                     },
                     {
@@ -188,22 +67,6 @@ const config = {
         },
         {
             resolve: "gatsby-plugin-catch-links",
-        },
-        {
-            resolve: "gatsby-plugin-react-svg",
-            options: {
-                rule: {
-                    include: /svg/,
-                },
-            },
-        },
-        {
-            resolve: "gatsby-plugin-layout",
-            options: {
-                component: require.resolve(
-                    "./src/components/Layout/Layout.tsx",
-                ),
-            },
         },
     ],
 }
