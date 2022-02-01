2751a13f9c51d95fbe1d2c4c839eccc94db6e1ba
Brad Garropy <bgarropy@atlassian.com>
2022-02-01 14:40:22 -0600

    make env variable public.

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -1,9 +1,9 @@
 import * as Sentry from "@sentry/nextjs"
 
-console.log(process.env.SENTRY_DSN)
+console.log(process.env.NEXT_PUBLIC_SENTRY_DSN)
 
 Sentry.init({
-    dsn: process.env.SENTRY_DSN,
+    dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
     tracesSampleRate: 1.0,
     debug: true,
 })
