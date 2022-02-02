3f4acc8e07e87ef8132c972a630a8584b001bff5
Brad Garropy <bgarropy@atlassian.com>
2022-02-02 09:09:55 -0600

    add log messages to api routes.

--- api/subscribe.ts
+++ api/subscribe.ts
@@ -2,25 +2,27 @@ import {post} from "@bradgarropy/http"
 import {withSentry} from "@sentry/nextjs"
 import {NextApiHandler} from "next"
 
+console.log(process.env.NEXT_PUBLIC_SENTRY_DSN)
+
 const handler: NextApiHandler = async (req, res) => {
     throw new Error("Sentry Backend Error | Subscribe")
 
-    const apiKey = process.env.REVUE_API_KEY
+    // const apiKey = process.env.REVUE_API_KEY
 
-    const subscriber = await post(
-        "https://www.getrevue.co/api/v2/subscribers",
-        {
-            headers: {
-                Authorization: `Token ${apiKey}`,
-            },
-            body: {
-                email: req.body.email,
-                double_opt_in: false,
-            },
-        },
-    )
+    // const subscriber = await post(
+    //     "https://www.getrevue.co/api/v2/subscribers",
+    //     {
+    //         headers: {
+    //             Authorization: `Token ${apiKey}`,
+    //         },
+    //         body: {
+    //             email: req.body.email,
+    //             double_opt_in: false,
+    //         },
+    //     },
+    // )
 
-    res.status(200).json(subscriber)
+    // res.status(200).json(subscriber)
 }
 
 export default withSentry(handler)
--- api/twitch.ts
+++ api/twitch.ts
@@ -2,6 +2,8 @@ import {get, post} from "@bradgarropy/http"
 import {withSentry} from "@sentry/nextjs"
 import {NextApiHandler, NextApiRequest, NextApiResponse} from "next"
 
+console.log(process.env.NEXT_PUBLIC_SENTRY_DSN)
+
 type ChannelStatus = {
     isLive: boolean
 }
@@ -9,30 +11,30 @@ type ChannelStatus = {
 const handler: NextApiHandler = async (req, res) => {
     throw new Error("Sentry Backend Error | Twitch")
 
-    const clientId = process.env.TWITCH_CLIENT_ID
-    const clientSecret = process.env.TWITCH_CLIENT_SECRET
+    // const clientId = process.env.TWITCH_CLIENT_ID
+    // const clientSecret = process.env.TWITCH_CLIENT_SECRET
 
-    const {access_token} = await post(
-        `https://id.twitch.tv/oauth2/token?client_id=${clientId}&client_secret=${clientSecret}&grant_type=client_credentials`,
-    )
+    // const {access_token} = await post(
+    //     `https://id.twitch.tv/oauth2/token?client_id=${clientId}&client_secret=${clientSecret}&grant_type=client_credentials`,
+    // )
 
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const channelResponse: any = await get(
-        "https://api.twitch.tv/helix/search/channels?query=bradgarropy",
-        {
-            headers: {
-                "authorization": `Bearer ${access_token}`,
-                "client-id": clientId,
-            },
-        },
-    )
+    // // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    // const channelResponse: any = await get(
+    //     "https://api.twitch.tv/helix/search/channels?query=bradgarropy",
+    //     {
+    //         headers: {
+    //             "authorization": `Bearer ${access_token}`,
+    //             "client-id": clientId,
+    //         },
+    //     },
+    // )
 
-    const channel = channelResponse.data.find(
-        channel => channel["broadcaster_login"] === "bradgarropy",
-    )
+    // const channel = channelResponse.data.find(
+    //     channel => channel["broadcaster_login"] === "bradgarropy",
+    // )
 
-    const channelStatus: ChannelStatus = {isLive: channel.is_live}
-    res.status(200).json(channelStatus)
+    // const channelStatus: ChannelStatus = {isLive: channel.is_live}
+    // res.status(200).json(channelStatus)
 }
 
 export default withSentry(handler)
