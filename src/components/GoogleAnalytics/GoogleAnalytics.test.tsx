99710169355fd6012fa83d5ab31e6a4767e151c3
Brad Garropy <bradgarropy@gmail.com>
2022-02-04 14:35:35 -0600

    switch to next-google-analytics package. (#275)
    Co-authored-by: Brad Garropy <bgarropy@atlassian.com>

--- src/components/GoogleAnalytics/GoogleAnalytics.test.tsx
+++
@@ -1,6 +0,0 @@
-import {render} from "@testing-library/react"
-import GoogleAnalytics from "components/GoogleAnalytics"
-
-test("initializes google analytics", () => {
-    render(<GoogleAnalytics measurementId="abc123" />)
-})
