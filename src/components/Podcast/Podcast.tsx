4d26df0c20a2df0e0e4c4778dc5f46f8e2fc02a7
Brad Garropy <bradgarropy@gmail.com>
2022-05-06 22:16:19 -0500

    switch to jpg.

--- src/components/Podcast/Podcast.tsx
+++ src/components/Podcast/Podcast.tsx
@@ -4,7 +4,7 @@ import Image from "next/image"
 import {FC} from "react"
 import {Podcast as PodcastType} from "types/podcast"
 
-import thumbnail from "../../../public/images/pages/home/web-dev-weekly.png"
+import thumbnail from "../../../public/images/pages/home/web-dev-weekly.jpg"
 
 type PodcastProps = {
     podcast: PodcastType
