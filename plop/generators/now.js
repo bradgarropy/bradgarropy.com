753a8c83424c0c20e056cf89e5cbd040b1d94a0b
Brad Garropy <bradgarropy@gmail.com>
2022-09-25 21:53:46 -0500

    Add plop generators for post and now.

---
+++ plop/generators/now.js
@@ -0,0 +1,18 @@
+const generator = plop => {
+    plop.load("plop-helper-date")
+
+    plop.setDefaultInclude({generators: true})
+
+    plop.setGenerator("now", {
+        prompts: [],
+        actions: [
+            {
+                type: "add",
+                path: "content/now/{{date}}.md",
+                templateFile: "../templates/now.hbs",
+            },
+        ],
+    })
+}
+
+module.exports = generator
--- plopfile.js
+++ plopfile.js
@@ -1,4 +1,7 @@
 const config = plop => {
+    plop.load("./plop/generators/post.js")
+    plop.load("./plop/generators/now.js")
+
     plop.load("@bradgarropy/plop-generator-react-component")
     plop.load("@bradgarropy/plop-generator-react-hook")
     plop.load("@bradgarropy/plop-generator-react-context")
