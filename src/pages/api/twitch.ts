6e3e834f07e0aa23bbd0f642c310fbafe20b9336
Brad Garropy <bgarropy@atlassian.com>
2022-09-27 09:27:47 -0500

    Adds types. Removes Sentry.

--- src/pages/api/twitch.ts
+++ src/pages/api/twitch.ts
@@ -1,21 +1,44 @@
 import {http} from "@bradgarropy/http"
-import {withSentry} from "@sentry/nextjs"
 import {NextApiHandler} from "next"
 
 type ChannelStatus = {
     isLive: boolean
 }
 
+type TwitchOauth = {
+    access_token: string
+    expires_in: number
+    refresh_token: string
+    scope: string[]
+    token_type: string
+}
+
+type TwitchSearch = {
+    data: TwitchChannel[]
+}
+
+type TwitchChannel = {
+    broadcaster_language: string
+    broadcaster_login: string
+    display_name: string
+    game_id: string
+    id: string
+    is_live: boolean
+    tag_ids: string[]
+    thumbnail_url: string
+    title: string
+    started_at: string
+}
+
 const handler: NextApiHandler = async (req, res) => {
     const clientId = process.env.TWITCH_CLIENT_ID
     const clientSecret = process.env.TWITCH_CLIENT_SECRET
 
-    const {access_token} = await http.post(
+    const {access_token} = await http.post<TwitchOauth>(
         `https://id.twitch.tv/oauth2/token?client_id=${clientId}&client_secret=${clientSecret}&grant_type=client_credentials`,
     )
 
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const channelResponse: any = await http.get(
+    const channelResponse = await http.get<TwitchSearch>(
         "https://api.twitch.tv/helix/search/channels?query=bradgarropy",
         {
             headers: {
@@ -33,4 +56,4 @@ const handler: NextApiHandler = async (req, res) => {
     res.status(200).json(channelStatus)
 }
 
-export default withSentry(handler)
+export default handler
