3a986c7157215993cf220b2b15bc6ba0c6396e09
Brad Garropy <bradgarropy@gmail.com>
2022-10-16 14:19:49 -0500

    🖼 rehype cloudinary image sizes (#333)
    Co-authored-by: Brad Garropy <bgarropy@atlassian.com>

--- src/utils/cloudinary.ts
+++ src/utils/cloudinary.ts
@@ -29,48 +29,9 @@ const createYouTubeUrl = (id: string) => {
     return url
 }
 
-type Dimensions = {
-    width: number
-    height: number
-}
-
-const getImageDimensions = async (imageUrl: string): Promise<Dimensions> => {
-    const url = new URL(imageUrl)
-
-    const pathSegments = url.pathname.split("/")
-    const index = pathSegments.findIndex(value => value === "bradgarropy.com")
-    pathSegments.splice(index, 0, "fl_getinfo")
-    url.pathname = pathSegments.join("/")
-
-    const response = await fetch(url)
-    const json = await response.json()
-
-    const dimensions: Dimensions = {
-        width: json.output.width,
-        height: json.output.height,
-    }
-
-    return dimensions
-}
-
-const isCloudinaryImage = (url: string) => {
-    if (url.startsWith("http://res.cloudinary.com")) {
-        return true
-    }
-
-    if (url.startsWith("https://res.cloudinary.com")) {
-        return true
-    }
-
-    return false
-}
-
 export {
     createExternalImageUrl,
     createImageUrl,
     createInternalImageUrl,
     createYouTubeUrl,
-    getImageDimensions,
-    isCloudinaryImage,
 }
-export type {Dimensions}
