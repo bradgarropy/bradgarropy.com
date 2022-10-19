c9a244246632e7681ee83096e0dc41df22efe4f4
Brad Garropy <bgarropy@atlassian.com>
2022-10-18 22:30:27 -0500

    coverage types.

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,7 +8,7 @@ const config = defineConfig({
     test: {
         coverage: {
             provider: "istanbul",
-            reporter: ["text", "json"],
+            reporter: ["text", "json", "clover", "lcov"],
         },
         css: {
             modules: {
