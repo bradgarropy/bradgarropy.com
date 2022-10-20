18f18c3ba97faa13c4ba4fe1b8ae0780ba62a295
Brad Garropy <bgarropy@atlassian.com>
2022-10-19 22:06:46 -0500

    install tailwind.

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+
+const config = {
+    content: ["src/pages/**/*.tsx", "src/components/**/*.tsx"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
+
+module.exports = config
