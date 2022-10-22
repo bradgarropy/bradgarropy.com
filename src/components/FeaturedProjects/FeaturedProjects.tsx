dde83d985f86d2a675fa79a5082182aa604fd3c4
Brad Garropy <bradgarropy@gmail.com>
2022-10-21 21:50:33 -0500

    featured projects component.

--- src/components/FeaturedProjects/FeaturedProjects.tsx
+++ src/components/FeaturedProjects/FeaturedProjects.tsx
@@ -2,15 +2,13 @@ import Project from "components/Project"
 import {FC} from "react"
 import {Project as ProjectType} from "types/project"
 
-import styles from "./FeaturedProjects.module.css"
-
 type FeaturedProjectsProps = {
     featuredProjects: ProjectType[]
 }
 
 const FeaturedProjects: FC<FeaturedProjectsProps> = ({featuredProjects}) => {
     return (
-        <div className={styles.featuredProjects}>
+        <div className="grid grid-cols-2 gap-7 max-[725px]:grid-cols-1">
             {featuredProjects.map(featuredProject => {
                 return (
                     <Project
