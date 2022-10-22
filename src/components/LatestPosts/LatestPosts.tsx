8445c6996e4a2fdf58019bbcefd463dab27c50df
Brad Garropy <bradgarropy@gmail.com>
2022-10-21 22:43:39 -0500

    latest posts component.

--- src/components/LatestPosts/LatestPosts.tsx
+++ src/components/LatestPosts/LatestPosts.tsx
@@ -2,15 +2,13 @@ import PostTitle from "components/PostTitle"
 import {FC} from "react"
 import {PostFrontmatter} from "types/post"
 
-import styles from "./LatestPosts.module.css"
-
 type LatestPostsProps = {
     latestPosts: PostFrontmatter[]
 }
 
 const LatestPosts: FC<LatestPostsProps> = ({latestPosts}) => {
     return (
-        <div className={styles.latestPosts}>
+        <div className="grid gap-y-6">
             {latestPosts.map(latestPost => {
                 return (
                     <PostTitle
