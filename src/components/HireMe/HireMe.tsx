82ffdb5fb0071a62b7d3a6b8bf70e68f532bd0aa
Brad Garropy <bradgarropy@gmail.com>
2022-10-21 22:37:15 -0500

    hire me component.

--- src/components/HireMe/HireMe.tsx
+++ src/components/HireMe/HireMe.tsx
@@ -1,4 +1,3 @@
-import classnames from "classnames"
 import Heading from "components/Heading"
 import Testimonials from "components/Testimonials"
 import {useMarkdown} from "hooks"
@@ -6,8 +5,6 @@ import {FC} from "react"
 import LinkStyles from "styles/Link.module.css"
 import {Markdown} from "types/markdown"
 import {Testimonial} from "types/testimonial"
-
-import styles from "./HireMe.module.css"
 
 type HireMeProps = {
     hireMe: Markdown
@@ -19,9 +16,7 @@ const HireMe: FC<HireMeProps> = ({hireMe, testimonials}) => {
 
     return (
         <>
-            <div className={classnames(LinkStyles.fancy, styles.hireMe)}>
-                {Markdown}
-            </div>
+            <div className={LinkStyles.fancy}>{Markdown}</div>
 
             <Heading level={1} id="testimonials">
                 💯 what people think of me
