8aea662f45fa0521e4f8d3f97eedc6b3b40d01ed
Brad Garropy <bgarropy@atlassian.com>
2022-12-07 21:55:46 -0600

    convert connect.

--- src/components/Connect/Connect.tsx
+++ src/components/Connect/Connect.tsx
@@ -2,25 +2,23 @@ import SocialLink from "components/SocialLink"
 import {FC} from "react"
 import {createImageUrl} from "utils/cloudinary"
 
-import styles from "./Connect.module.css"
-
 const Connect: FC = () => {
     return (
-        <div className={styles.connect}>
+        <div className="grid gap-8 text-xl text-center">
             <img
                 src={createImageUrl("/pages/home/profile.jpg")}
                 alt="bg"
-                className={styles.profile}
+                className="rounded-full border-8 border-solid border-black w-4/5"
                 width="460"
                 height="460"
             />
 
             <div>
-                <p>Thanks for chatting with me!</p>
-                <p>Let&apos;s connect on these platforms.</p>
+                <p className="m-0">Thanks for chatting with me!</p>
+                <p className="m-0">Let&apos;s connect on these platforms.</p>
             </div>
 
-            <div className={styles.socials}>
+            <div className="grid grid-flow-col justify-center content-center gap-x-5">
                 <SocialLink platform="twitch" />
                 <SocialLink platform="github" />
                 <SocialLink platform="youtube" />
