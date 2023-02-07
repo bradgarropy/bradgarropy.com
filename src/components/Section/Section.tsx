ac9339afa6dd085c9ef37bb7b22cf14f6c4b72a6
Brad Garropy <bgarropy@atlassian.com>
2023-02-07 14:16:26 -0600

    remove section.

--- src/components/Section/Section.tsx
+++ src/components/Section/Section.tsx
@@ -1,22 +1,32 @@
 import Link from "@bradgarropy/next-link"
 import {FC, HTMLAttributes} from "react"
 
-import SectionStyles from "./Section.module.css"
-
 type SectionProps = {
     title: string
     link?: string
 } & HTMLAttributes<HTMLDivElement>
 
+type TitleProps = {
+    title: string
+}
+
+const Title: FC<TitleProps> = ({title}) => {
+    return (
+        <h2 className="transition-[text-shadow] duration-300 text-[clamp(3rem,_10vw,_4.5rem)] font-black tracking-[-0.2rem] m-0 hover:[text-shadow:_3px_3px_var(--primary)]">
+            {title}
+        </h2>
+    )
+}
+
 const Section: FC<SectionProps> = ({title, link, children, ...props}) => {
     return (
         <section {...props}>
             {link ? (
-                <Link className={SectionStyles.link} to={link}>
-                    <h2 className={SectionStyles.title}>{title}</h2>
+                <Link className="inline-block m-0 mb-7" to={link}>
+                    <Title title={title} />
                 </Link>
             ) : (
-                <h2 className={SectionStyles.title}>{title}</h2>
+                <Title title={title} />
             )}
 
             {children}
