4d89fb07e7ec42ba30b0a05fa3d831cd9fb5d3d8
Brad Garropy <bradgarropy@gmail.com>
2023-02-22 15:12:29 -0600

    Remove markdown (#347)
    Co-authored-by: Brad Garropy <bgarropy@atlassian.com>

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,21 +1,38 @@
-import Markdown from "components/Markdown"
-import {useMarkdown} from "hooks"
+import FancyLink from "components/FancyLink"
 import {FC} from "react"
-import {Markdown as MarkdownType} from "types/markdown"
 
-type ContactProps = {
-    contact: MarkdownType
-}
-
-const Contact: FC<ContactProps> = ({contact}) => {
-    const markdown = useMarkdown(contact.html)
-
+const Contact: FC = () => {
     return (
         <div className="place-self-center">
             <h1 className="mb-7 font-heading text-[2rem] font-semibold">
                 👋🏼 say hi
             </h1>
-            <Markdown content={markdown} />
+
+            <p className="mb-5">
+                I enjoy connecting with other developers, drop me a message and
+                let&apos;s talk!
+            </p>
+
+            <p>Social media is great for informal chats.</p>
+            <p>Email works best for in depth discussions.</p>
+            <p>Consider my AMA if others can benefit from the question.</p>
+
+            <div className="mt-8 grid grid-cols-[repeat(2,auto)] justify-start justify-items-start gap-5 text-2xl">
+                <span>📧</span>
+                <FancyLink to="mailto:bradgarropy@gmail.com">
+                    bradgarropy@gmail.com
+                </FancyLink>
+
+                <span>🙋🏼‍♂️</span>
+                <FancyLink to="https://bradgarropy.com/ama">
+                    ask me anything
+                </FancyLink>
+
+                <span>🐦</span>
+                <FancyLink to="https://twitter.com/bradgarropy">
+                    twitter
+                </FancyLink>
+            </div>
         </div>
     )
 }
--- src/components/HireMe/HireMe.tsx
+++ src/components/HireMe/HireMe.tsx
@@ -1,22 +1,42 @@
+import FancyLink from "components/FancyLink"
 import Heading from "components/Heading"
-import Markdown from "components/Markdown"
 import Testimonials from "components/Testimonials"
-import {useMarkdown} from "hooks"
 import {FC} from "react"
-import {Markdown as MarkdownType} from "types/markdown"
 import {Testimonial} from "types/testimonial"
 
 type HireMeProps = {
-    hireMe: MarkdownType
     testimonials: Testimonial[]
 }
 
-const HireMe: FC<HireMeProps> = ({hireMe, testimonials}) => {
-    const markdown = useMarkdown(hireMe.html)
-
+const HireMe: FC<HireMeProps> = ({testimonials}) => {
     return (
         <div className="grid gap-y-9">
-            <Markdown content={markdown} />
+            <h1 className="font-heading text-4xl font-extrabold">
+                🤝 let&apos;s work together
+            </h1>
+
+            <p>
+                I just snagged a new job at{" "}
+                <FancyLink to="https://atlassian.com">
+                    <span className="font-semibold">Atlassian</span>
+                </FancyLink>
+                , working on{" "}
+                <FancyLink to="https://trello.com">
+                    <span className="font-semibold">Trello</span>
+                </FancyLink>{" "}
+                as a{" "}
+                <span className="italic">Developer Experience Engineer</span>.
+                Although I&apos;m not actively looking for a new role, please
+                check out my{" "}
+                <FancyLink to="https://bradgarropy.com/resume">
+                    <span className="font-semibold">resume</span>
+                </FancyLink>{" "}
+                and{" "}
+                <FancyLink to="https://bradgarropy.com/contact">
+                    <span className="font-semibold">contact</span>
+                </FancyLink>{" "}
+                me!
+            </p>
 
             <div className="prose prose-h1:font-bold dark:prose-invert">
                 <Heading level={1} id="testimonials">
