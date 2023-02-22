59e0c57fec029d529c630ad90116de2fbcd3c6db
Brad Garropy <bgarropy@atlassian.com>
2023-02-22 14:50:17 -0600

    remove contact markdown.

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,33 +1,15 @@
 import SEO from "@bradgarropy/next-seo"
 import Contact from "components/Contact"
 import Layout from "components/Layout"
-import {GetStaticProps} from "next"
 import {FC} from "react"
-import {Markdown} from "types/markdown"
-import {getMarkdownBySlug} from "utils/markdown"
 
-type ContactPageProps = {
-    contact: Markdown
-}
-
-const ContactPage: FC<ContactPageProps> = ({contact}) => {
+const ContactPage: FC = () => {
     return (
         <Layout>
             <SEO title="👋🏼 say hi" />
-            <Contact contact={contact} />
+            <Contact />
         </Layout>
     )
 }
 
-const getStaticProps: GetStaticProps = async () => {
-    const contact = await getMarkdownBySlug("contact")
-
-    return {
-        props: {
-            contact,
-        },
-    }
-}
-
 export default ContactPage
-export {getStaticProps}
