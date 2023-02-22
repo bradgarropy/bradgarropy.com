3d601176476be58bb9c68135e594b22f963a5ec9
Brad Garropy <bgarropy@atlassian.com>
2023-02-22 10:21:08 -0600

    fix twitter tests.

--- src/utils/markdown.test.ts
+++ src/utils/markdown.test.ts
@@ -114,8 +114,10 @@ describe("transforms markdown", () => {
             "https://twitter.com/bradgarropy/status/1458449938157801490",
         )
 
-        // eslint-disable-next-line quotes
-        expect(html).toEqual(expect.stringContaining('<div class="twitter">'))
+        expect(html).toEqual(
+            // eslint-disable-next-line quotes
+            expect.stringContaining('<div class="grid justify-center">'),
+        )
 
         expect(html).toEqual(
             // eslint-disable-next-line quotes
