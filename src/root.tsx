b6b21fbe97c4b9753b18414dbe9530b172d37640
Brad Garropy <bgarropy@atlassian.com>
2023-03-01 22:39:24 -0600

    add fonts.

--- src/root.tsx
+++ src/root.tsx
@@ -32,6 +32,19 @@ const App = () => {
     return (
         <html lang="en">
             <head>
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+
+                <link
+                    rel="preconnect"
+                    href="https://fonts.gstatic.com"
+                    crossOrigin="anonymous"
+                />
+
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Righteous&family=Open+Sans:wght@400;500;600;700;800&family=Montserrat:wght@400;500;600;700;800;900&display=swap"
+                    rel="stylesheet"
+                />
+
                 <Meta />
                 <Links />
             </head>
