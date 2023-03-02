94e846e077d01206c8f383be2696fdc3263a1eb7
Brad Garropy <bgarropy@atlassian.com>
2023-03-01 21:27:34 -0600

    remove svg shim.

--- jest.config.js
+++ jest.config.js
@@ -7,7 +7,6 @@ const config = {
         "\\.module.css$": "identity-obj-proxy",
         "\\.css$": "<rootDir>/src/test-utils/css.ts",
         "\\.jpg$": "<rootDir>/src/test-utils/image.ts",
-        "\\.svg$": "<rootDir>/src/test-utils/svg.tsx",
     },
     testEnvironment: "jsdom",
     transform: {
