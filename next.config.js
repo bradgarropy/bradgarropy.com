99eff28e9d53ba9bec0f0a1b95c1e455da157acd
Brad Garropy <bgarropy@atlassian.com>
2023-03-01 21:27:40 -0600

    remove svg webpack loader.

--- next.config.js
+++ next.config.js
@@ -79,21 +79,6 @@ const config = {
 
         return redirects
     },
-    webpack: config => {
-        config.module.rules.push({
-            test: /\.svg$/,
-            use: [
-                {
-                    loader: "@svgr/webpack",
-                    options: {
-                        svgo: false,
-                    },
-                },
-            ],
-        })
-
-        return config
-    },
 }
 
 module.exports = config
