ff8af66bd9d5cb5f9034f1d19df31a6b65e70cf6
Brad Garropy <bgarropy@atlassian.com>
2023-03-24 11:05:22 -0500

    flat routes.

--- remix.config.js
+++ remix.config.js
@@ -4,7 +4,7 @@ const config = {
     appDirectory: "src",
     future: {
         unstable_tailwind: true,
-        // v2_routeConvention: true,
+        v2_routeConvention: true,
     },
     ignoredRouteFiles: ["**/.*"],
     server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
