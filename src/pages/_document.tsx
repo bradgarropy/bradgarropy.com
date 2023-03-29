ddea3f67123a3b0ec41bf745c36e8fc2aa1826f1
Brad Garropy <bgarropy@atlassian.com>
2023-03-29 00:24:22 -0500

    variable fonts.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,20 +4,7 @@ import Script from "next/script"
 const Document = () => {
     return (
         <Html>
-            <Head>
-                <link rel="preconnect" href="https://fonts.googleapis.com" />
-
-                <link
-                    rel="preconnect"
-                    href="https://fonts.gstatic.com"
-                    crossOrigin="true"
-                />
-
-                <link
-                    href="https://fonts.googleapis.com/css2?family=Righteous&family=Open+Sans:wght@400;500;600;700;800&family=Montserrat:wght@400;500;600;700;800;900&display=swap"
-                    rel="stylesheet"
-                />
-            </Head>
+            <Head />
 
             <body className="bg-white transition duration-300 dark:bg-black">
                 <Main />
