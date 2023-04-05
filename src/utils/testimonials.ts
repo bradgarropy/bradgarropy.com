a49a98f5b6c58d5dbde0a4f0eff4c1b80eefa8de
Brad Garropy <bgarropy@atlassian.com>
2023-04-05 11:58:46 -0500

    convert testimonials.

--- src/utils/testimonials.ts
+++ src/utils/testimonials.ts
@@ -1,35 +1,16 @@
-import fs from "fs"
-import matter from "gray-matter"
-import path from "path"
-
-import type {Testimonial, TestimonialFrontmatter} from "~/types/testimonial"
-import {transformMarkdown} from "~/utils/markdown"
+import {allTestimonials} from "~/content"
+import type {Testimonial} from "~/types/testimonial"
 
 const getTestimonials = async (): Promise<Testimonial[]> => {
-    const testimonialsPath = path.join(process.cwd(), "content/testimonials")
+    const testimonials: Testimonial[] = allTestimonials.map(testimonial => ({
+        html: testimonial.body.html,
+        frontmatter: {
+            name: testimonial.name,
+            profile: testimonial.profile,
+            photo: testimonial.photo,
+        },
+    }))
 
-    const testimonialPaths = fs.readdirSync(testimonialsPath).map(slug => {
-        const testimonialPath = path.join(
-            process.cwd(),
-            `content/testimonials/${slug}`,
-        )
-
-        return testimonialPath
-    })
-
-    const promises = testimonialPaths.map(async testimonialPath => {
-        const file = matter.read(testimonialPath)
-        const html = await transformMarkdown(file.content)
-
-        const testimonial: Testimonial = {
-            html,
-            frontmatter: file.data as TestimonialFrontmatter,
-        }
-
-        return testimonial
-    })
-
-    const testimonials = await Promise.all(promises)
     return testimonials
 }
 
