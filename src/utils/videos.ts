29d3d54d009042a003a60dd7e40127d5b64afb7e
Brad Garropy <bgarropy@atlassian.com>
2023-09-13 13:54:43 -0500

    add count to getLatestVideos.

--- src/utils/videos.ts
+++ src/utils/videos.ts
@@ -56,14 +56,14 @@ type Thumbnail = {
     height: number
 }
 
-const getLatestVideos = async (): Promise<Video[]> => {
+const getLatestVideos = async (count = 2): Promise<Video[]> => {
     const response = await http.get<YouTubeSearchResponse>(
         "https://www.googleapis.com/youtube/v3/search",
         {
             params: {
                 key: process.env.YOUTUBE_API_KEY,
                 channelId: "UCgbFhcZKt36Upo7oxWlLEig",
-                maxResults: 2,
+                maxResults: count,
                 part: "snippet",
                 order: "date",
                 type: "video",
