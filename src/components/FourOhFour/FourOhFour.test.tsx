c25b8378af8fd3a9f4e237b092a1613ffcfe9dfe
Brad Garropy <bgarropy@atlassian.com>
2023-09-13 15:13:56 -0500

    add content to 404 page.

--- src/components/FourOhFour/FourOhFour.test.tsx
+++ src/components/FourOhFour/FourOhFour.test.tsx
@@ -1,8 +1,15 @@
 import {render, screen} from "@testing-library/react"
 
 import FourOhFour from "~/components/FourOhFour"
+import {mockPostFrontmatter, mockVideos} from "~/test-utils/mocks"
 
 test("shows 404", () => {
-    render(<FourOhFour />)
-    expect(screen.getByText("404"))
+    render(<FourOhFour post={mockPostFrontmatter} videos={mockVideos} />)
+
+    expect(screen.getByText("There's nothing here."))
+    expect(screen.getByText(mockPostFrontmatter.title))
+
+    mockVideos.forEach(mockVideo => {
+        expect(screen.getByAltText(mockVideo.title))
+    })
 })
--- src/components/FourOhFour/FourOhFour.tsx
+++ src/components/FourOhFour/FourOhFour.tsx
@@ -1,9 +1,31 @@
 import type {FC} from "react"
 
-const FourOhFour: FC = () => {
+import LatestVideos from "~/components/LatestVideos"
+import PostCard from "~/components/PostCard"
+import type {PostFrontmatter} from "~/types/post"
+import type {Video} from "~/types/video"
+
+type FourOhFourProps = {
+    post: PostFrontmatter
+    videos: Video[]
+}
+
+const FourOhFour: FC<FourOhFourProps> = ({post, videos}) => {
     return (
-        <div className="grid place-items-center">
-            <h1 className="font-heading text-9xl">404</h1>
+        <div className="grid h-full content-center">
+            <h1 className="mb-2 font-heading text-6xl font-black tracking-[-0.2rem]">
+                There&apos;s nothing here.
+            </h1>
+
+            <p className="mb-12">
+                But maybe you were looking for some of my recent content?
+            </p>
+
+            <div className="mb-4 w-full">
+                <PostCard post={post} />
+            </div>
+
+            <LatestVideos latestVideos={videos} />
         </div>
     )
 }
