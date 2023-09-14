0ba613fdfc0be778c539dd76a4b5064f2c2816d8
Brad Garropy <bgarropy@atlassian.com>
2023-09-14 15:55:22 -0500

    change to loops.

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -2,17 +2,17 @@ import {http} from "@bradgarropy/http"
 import type {NextApiHandler} from "next"
 
 const handler: NextApiHandler = async (req, res) => {
-    const apiKey = process.env.REVUE_API_KEY
+    const apiKey = process.env.LOOPS_API_KEY
 
     const subscriber = await http.post(
-        "https://www.getrevue.co/api/v2/subscribers",
+        "https://app.loops.so/api/v1/contacts/create",
         {
             headers: {
-                Authorization: `Token ${apiKey}`,
+                Authorization: `Bearer ${apiKey}`,
             },
             body: {
                 email: req.body.email,
-                double_opt_in: false,
+                source: "bradgarropy.com",
             },
         },
     )
