2849c7e234c5919aabbddbd4fd0cc89c577935b5
Brad Garropy <bradgarropy@gmail.com>
2023-11-16 15:56:11 -0600

    done with blog page.

--- src/routes/blog.$slug.tsx
+++ src/routes/blog.$slug.tsx
@@ -1,13 +1,47 @@
-import type {MetaFunction} from "@remix-run/node"
+import type {LoaderFunctionArgs, MetaFunction} from "@remix-run/node"
+import {json} from "@remix-run/node"
+import {useLoaderData} from "@remix-run/react"
 
-export const meta: MetaFunction = () => [
+import Layout from "~/components/Layout"
+import Newsletter from "~/components/Newsletter"
+import Post from "~/components/Post"
+import PostList from "~/components/PostList"
+import Section from "~/components/Section"
+import type {PostFrontmatter} from "~/types/post"
+import {getPostBySlug, getRelatedPosts} from "~/utils/posts"
+
+export const loader = async ({params}: LoaderFunctionArgs) => {
+    const slug = params.slug as PostFrontmatter["slug"]
+    const post = await getPostBySlug(slug)
+    const relatedPosts = getRelatedPosts(post.frontmatter)
+
+    return json({post, relatedPosts})
+}
+
+export const meta: MetaFunction<typeof loader> = ({data}) => [
     {
-        title: "💿 remix starter | blog slug",
+        title: data?.post.frontmatter.title,
     },
 ]
 
 const BlogRoute = () => {
-    return <h2 className="text-2xl font-bold">blog slug</h2>
+    const {post, relatedPosts} = useLoaderData<typeof loader>()
+
+    return (
+        <Layout>
+            <div className="flex flex-col gap-y-28">
+                <Post post={post} />
+
+                <Section id="related-posts" title="related posts">
+                    <PostList posts={relatedPosts} />
+                </Section>
+
+                <Section id="newsletter" title="newsletter" link="/newsletter">
+                    <Newsletter />
+                </Section>
+            </div>
+        </Layout>
+    )
 }
 
 export default BlogRoute
