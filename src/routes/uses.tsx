d4b903772936a6c8befdf26e24a70066f716d56b
Brad Garropy <bradgarropy@gmail.com>
2023-11-16 15:52:14 -0600

    done with uses page.

--- src/routes/uses.tsx
+++ src/routes/uses.tsx
@@ -1,13 +1,29 @@
-import type {MetaFunction} from "@remix-run/node"
+import {json, type MetaFunction} from "@remix-run/node"
+import {useLoaderData} from "@remix-run/react"
+
+import Layout from "~/components/Layout"
+import Uses from "~/components/Uses"
+import {getMarkdownBySlug} from "~/utils/markdown"
+
+export const loader = async () => {
+    const uses = await getMarkdownBySlug("uses")
+    return json({uses})
+}
 
 export const meta: MetaFunction = () => [
     {
-        title: "💿 remix starter | uses",
+        title: "💠 uses",
     },
 ]
 
 const UsesRoute = () => {
-    return <h2 className="text-2xl font-bold">uses</h2>
+    const {uses} = useLoaderData<typeof loader>()
+
+    return (
+        <Layout>
+            <Uses uses={uses} />
+        </Layout>
+    )
 }
 
 export default UsesRoute
