5f5b8dca4b4e7e1912d76a548e00a66c0dd78f50
Brad Garropy <bradgarropy@gmail.com>
2023-11-16 13:31:49 -0600

    done with home page.

--- src/routes/_index.tsx
+++ src/routes/_index.tsx
@@ -1,4 +1,23 @@
-import type {MetaFunction} from "@remix-run/node"
+import {json, type MetaFunction} from "@remix-run/node"
+import {useLoaderData} from "@remix-run/react"
+
+import Home from "~/components/Home"
+import Layout from "~/components/Layout"
+import {getLatestPosts} from "~/utils/posts"
+import {getFeaturedProjects} from "~/utils/projects"
+import {getLatestVideos} from "~/utils/videos"
+
+export const loader = async () => {
+    const latestPosts = getLatestPosts(3)
+    const latestVideos = await getLatestVideos(2)
+    const featuredProjects = await getFeaturedProjects()
+
+    return json({
+        latestPosts,
+        latestVideos,
+        featuredProjects,
+    })
+}
 
 export const meta: MetaFunction = () => [
     {
@@ -7,7 +26,18 @@ export const meta: MetaFunction = () => [
 ]
 
 const IndexRoute = () => {
-    return <h2 className="text-2xl font-bold">Home</h2>
+    const {latestPosts, latestVideos, featuredProjects} =
+        useLoaderData<typeof loader>()
+
+    return (
+        <Layout>
+            <Home
+                latestPosts={latestPosts}
+                latestVideos={latestVideos}
+                featuredProjects={featuredProjects}
+            />
+        </Layout>
+    )
 }
 
 export default IndexRoute
