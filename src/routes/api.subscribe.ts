08952893873d2ad6a3f148fc80cfebf165d108d8
Brad Garropy <bradgarropy@gmail.com>
2023-11-16 13:32:10 -0600

    done with api routes.

---
+++ src/routes/api.subscribe.ts
@@ -0,0 +1,22 @@
+import {http} from "@bradgarropy/http"
+import {type ActionFunctionArgs, json} from "@remix-run/node"
+
+export const action = async ({request}: ActionFunctionArgs) => {
+    const {email} = await request.json()
+    const apiKey = process.env.LOOPS_API_KEY
+
+    const subscriber = await http.post(
+        "https://app.loops.so/api/v1/contacts/create",
+        {
+            headers: {
+                Authorization: `Bearer ${apiKey}`,
+            },
+            body: {
+                email,
+                source: "bradgarropy.com",
+            },
+        },
+    )
+
+    return json(subscriber)
+}
---
+++ src/routes/api.twitch.ts
@@ -0,0 +1,63 @@
+import {http} from "@bradgarropy/http"
+import {json} from "@remix-run/node"
+
+type ChannelStatus = {
+    isLive: boolean
+}
+
+type TwitchOauth = {
+    access_token: string
+    expires_in: number
+    refresh_token: string
+    scope: string[]
+    token_type: string
+}
+
+type TwitchSearch = {
+    data: TwitchChannel[]
+}
+
+type TwitchChannel = {
+    broadcaster_language: string
+    broadcaster_login: string
+    display_name: string
+    game_id: string
+    id: string
+    is_live: boolean
+    tag_ids: string[]
+    thumbnail_url: string
+    title: string
+    started_at: string
+}
+
+export const loader = async () => {
+    const clientId = process.env.TWITCH_CLIENT_ID
+    const clientSecret = process.env.TWITCH_CLIENT_SECRET
+
+    const {access_token} = await http.post<TwitchOauth>(
+        `https://id.twitch.tv/oauth2/token?client_id=${clientId}&client_secret=${clientSecret}&grant_type=client_credentials`,
+    )
+
+    const channelResponse = await http.get<TwitchSearch>(
+        "https://api.twitch.tv/helix/search/channels?query=bradgarropy",
+        {
+            headers: {
+                "authorization": `Bearer ${access_token}`,
+                "client-id": clientId,
+            },
+        },
+    )
+
+    const channel = channelResponse.data.find(
+        channel => channel["broadcaster_login"] === "bradgarropy",
+    )
+
+    if (!channel) {
+        return json("Not found", {status: 404})
+    }
+
+    const channelStatus: ChannelStatus = {isLive: channel.is_live}
+    return json(channelStatus)
+}
+
+export type {ChannelStatus}
