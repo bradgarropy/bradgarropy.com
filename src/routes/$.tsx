1115b84a11360aa8038cdea4dd1d247748421c10
Brad Garropy <bradgarropy@gmail.com>
2023-11-16 20:41:40 -0600

    done with 404 page.

---
+++ src/routes/$.tsx
@@ -0,0 +1,36 @@
+import type {MetaFunction} from "@remix-run/node"
+import {json} from "@remix-run/node"
+import {useLoaderData} from "@remix-run/react"
+
+import FourOhFour from "~/components/FourOhFour"
+import Layout from "~/components/Layout"
+import {getLatestPost} from "~/utils/posts"
+import {getLatestVideos} from "~/utils/videos"
+
+export const loader = async () => {
+    const latestPost = getLatestPost()
+    const latestVideos = await getLatestVideos(2)
+
+    return json({
+        latestPost,
+        latestVideos,
+    })
+}
+
+export const meta: MetaFunction = () => [
+    {
+        title: "🤷🏼‍♂️ not found",
+    },
+]
+
+const NotFoundRoute = () => {
+    const {latestPost, latestVideos} = useLoaderData<typeof loader>()
+
+    return (
+        <Layout>
+            <FourOhFour post={latestPost} videos={latestVideos} />
+        </Layout>
+    )
+}
+
+export default NotFoundRoute
