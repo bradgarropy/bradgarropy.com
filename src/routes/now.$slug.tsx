f99f64a39a3a1001ef2582b81daffe6c14c2f439
Brad Garropy <bradgarropy@gmail.com>
2023-11-16 20:35:41 -0600

    done with now page.

--- src/routes/now.$slug.tsx
+++ src/routes/now.$slug.tsx
@@ -1,13 +1,40 @@
-import type {MetaFunction} from "@remix-run/node"
+import type {LoaderFunctionArgs, MetaFunction} from "@remix-run/node"
+import {json} from "@remix-run/node"
+import {useLoaderData} from "@remix-run/react"
+
+import Layout from "~/components/Layout"
+import Now from "~/components/Now"
+import type {NowFrontmatter} from "~/types/now"
+import {getNewerNow, getNowByDate, getOlderNow} from "~/utils/now"
+
+export const loader = async ({params}: LoaderFunctionArgs) => {
+    const date = params.slug as NowFrontmatter["date"]
+
+    const currentNow = await getNowByDate(date)
+    const newerNow = await getNewerNow(currentNow)
+    const olderNow = await getOlderNow(currentNow)
+
+    return json({
+        currentNow,
+        newerNow,
+        olderNow,
+    })
+}
 
 export const meta: MetaFunction = () => [
     {
-        title: "💿 remix starter | now slug",
+        title: "🧭 now",
     },
 ]
 
 const NowRoute = () => {
-    return <h2 className="text-2xl font-bold">now slug</h2>
+    const {currentNow, newerNow, olderNow} = useLoaderData<typeof loader>()
+
+    return (
+        <Layout>
+            <Now now={currentNow} newer={newerNow} older={olderNow} />
+        </Layout>
+    )
 }
 
 export default NowRoute
