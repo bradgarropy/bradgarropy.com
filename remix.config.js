6402168d6131097d3365cc3a9a6d2ce597b2ed10
Brad Garropy <bradgarropy@gmail.com>
2023-11-27 22:42:20 -0600

    fix build process.

--- remix.config.js
+++ remix.config.js
@@ -4,8 +4,6 @@ const config = {
     appDirectory: "src",
     ignoredRouteFiles: ["**/.*"],
     future: {},
-    server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
-    serverBuildPath: "api/index.js",
     serverModuleFormat: "esm",
 }
 
