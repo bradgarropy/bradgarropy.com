524216661e79a1b9a9dea45b3141e6f9ab1cfc75
Brad Garropy <bradgarropy@gmail.com>
2023-11-29 13:08:51 -0600

    copy theme to public.

--- src/scripts/copyTheme.ts
+++ src/scripts/copyTheme.ts
@@ -2,5 +2,5 @@ import {copyFileSync} from "node:fs"
 
 copyFileSync(
     "node_modules/shades-of-purple/themes/shades-of-purple-color-theme.json",
-    "src/styles/theme.json",
+    "public/theme.json",
 )
