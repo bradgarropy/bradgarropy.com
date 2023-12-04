c5fa8a4af9605adf14d44102d9e0ec7d74e1e0bd
Brad Garropy <bradgarropy@gmail.com>
2023-12-04 10:11:21 -0600

    fix maxage header.

--- src/routes/api.youtube.ts
+++ src/routes/api.youtube.ts
@@ -4,7 +4,7 @@ import {getLatestVideos} from "~/utils/videos"
 
 export const loader = async () => {
     const latestVideos = await getLatestVideos()
-    const headers = {"Cache-Control": "public, s-max-age=86400"}
+    const headers = {"Cache-Control": "public, s-maxage=86400"}
 
     return json({latestVideos}, {headers})
 }
