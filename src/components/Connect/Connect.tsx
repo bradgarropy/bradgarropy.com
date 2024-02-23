f958201303d8b809d25351aa7d272e2c71ea76bc
Brad Garropy <bradgarropy@gmail.com>
2024-02-23 15:25:37 -0600

    add hover effect to connect image.

--- src/components/Connect/Connect.tsx
+++ src/components/Connect/Connect.tsx
@@ -1,3 +1,4 @@
+import {Link} from "@remix-run/react"
 import type {FC} from "react"
 
 import SocialLink from "~/components/SocialLink"
@@ -6,13 +7,15 @@ import {createImageUrl} from "~/utils/cloudinary"
 const Connect: FC = () => {
     return (
         <div className="grid justify-items-center gap-8 text-center text-xl">
-            <img
-                src={createImageUrl("/pages/home/profile.jpg")}
-                alt="bg"
-                className="w-[80vw] max-w-[14rem] rounded-full border-7 border-solid border-black dark:border-white"
-                width="460"
-                height="460"
-            />
+            <Link to="/">
+                <img
+                    src={createImageUrl("/pages/home/profile.jpg")}
+                    alt="bg"
+                    className="w-[80vw] max-w-[14rem] transform rounded-full border-7 border-solid border-black duration-300 hover:-rotate-2 hover:border-purple-400 dark:border-white dark:hover:border-purple-400"
+                    width="460"
+                    height="460"
+                />
+            </Link>
 
             <div>
                 <p className="m-0">Thanks for chatting with me!</p>
