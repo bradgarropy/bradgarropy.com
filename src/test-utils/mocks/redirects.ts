7140570f82ddc2d0d17e5d6974cd9af55bd5f770
Brad Garropy <bradgarropy@gmail.com>
2024-02-23 08:48:06 -0600

    ➡️ custom redirects (#413)

---
+++ src/test-utils/mocks/redirects.ts
@@ -0,0 +1,14 @@
+const mockRedirects = [
+    {
+        source: "/permanent",
+        destination: "https://permanent.com",
+        permanent: true,
+    },
+    {
+        source: "/temporary",
+        destination: "https://temporary.com",
+        permanent: false,
+    },
+]
+
+export {mockRedirects}
