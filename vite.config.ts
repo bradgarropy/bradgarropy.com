d7eff774f42846a2c7273feceeff1b721ca2ef3b
Brad Garropy <bradgarropy@gmail.com>
2024-03-04 12:51:27 -0600

    use vercel vite preset.

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import {vitePlugin as remix} from "@remix-run/dev"
 import {installGlobals} from "@remix-run/node"
+import {vercelPreset} from "@vercel/remix/vite"
 import {defineConfig} from "vite"
 import tsconfigPaths from "vite-tsconfig-paths"
 
@@ -12,6 +13,7 @@ export default defineConfig({
             appDirectory: "src",
             ignoredRouteFiles: ["**/.*"],
             future: {},
+            presets: [vercelPreset()],
             serverModuleFormat: "esm",
         }),
     ],
