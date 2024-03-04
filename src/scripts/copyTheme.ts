fc8fde6a53186da57c9eb57e3666038c013edc97
Brad Garropy <bradgarropy@gmail.com>
2024-03-04 13:16:04 -0600

    change shikiji path.

--- src/scripts/copyTheme.ts
+++ src/scripts/copyTheme.ts
@@ -1,14 +1,14 @@
 import {copyFileSync, cpSync} from "node:fs"
 
-cpSync("node_modules/shikiji/dist/themes", "build/shikiji/themes", {
+cpSync("node_modules/shikiji/dist/themes", "build/server/shikiji/themes", {
     recursive: true,
 })
 
-cpSync("node_modules/shikiji/dist/langs", "build/shikiji/langs", {
+cpSync("node_modules/shikiji/dist/langs", "build/server/shikiji/langs", {
     recursive: true,
 })
 
 copyFileSync(
     "node_modules/shades-of-purple/themes/shades-of-purple-color-theme.json",
-    "build/shikiji/themes/shades-of-purple.json",
+    "build/server/shikiji/themes/shades-of-purple.json",
 )
--- src/utils/markdown.server.ts
+++ src/utils/markdown.server.ts
@@ -42,7 +42,7 @@ const getMarkdownBySlug = async (slug: string): Promise<Markdown> => {
 const transformMarkdown = async (markdown: string): Promise<string> => {
     const themePath = path.join(
         process.cwd(),
-        "build/shikiji/themes/shades-of-purple.json",
+        "build/server/shikiji/themes/shades-of-purple.json",
     )
 
     const theme = fs.readFileSync(themePath, "utf8")
@@ -56,8 +56,14 @@ const transformMarkdown = async (markdown: string): Promise<string> => {
                 // @ts-expect-error
                 theme: json5.parse(theme),
                 paths: {
-                    languages: path.join(process.cwd(), "build/shikiji/langs"),
-                    themes: path.join(process.cwd(), "build/shikiji/themes"),
+                    languages: path.join(
+                        process.cwd(),
+                        "build/server/shikiji/langs",
+                    ),
+                    themes: path.join(
+                        process.cwd(),
+                        "build/server/shikiji/themes",
+                    ),
                 },
             }),
     }
