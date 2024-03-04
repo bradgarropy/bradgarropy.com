a865297470399a53f99682c8c58cbc77c01a5ea5
Brad Garropy <bradgarropy@gmail.com>
2024-03-04 13:58:54 -0600

    copy to client.

--- src/scripts/copyTheme.ts
+++ src/scripts/copyTheme.ts
@@ -1,14 +1,14 @@
 import {copyFileSync, cpSync} from "node:fs"
 
-cpSync("node_modules/shikiji/dist/themes", "build/server/shikiji/themes", {
+cpSync("node_modules/shikiji/dist/themes", "build/client/shikiji/themes", {
     recursive: true,
 })
 
-cpSync("node_modules/shikiji/dist/langs", "build/server/shikiji/langs", {
+cpSync("node_modules/shikiji/dist/langs", "build/client/shikiji/langs", {
     recursive: true,
 })
 
 copyFileSync(
     "node_modules/shades-of-purple/themes/shades-of-purple-color-theme.json",
-    "build/server/shikiji/themes/shades-of-purple.json",
+    "build/client/shikiji/themes/shades-of-purple.json",
 )
