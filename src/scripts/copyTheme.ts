6aaf9611921eb25131b34a2a319a905b9c9713e7
Brad Garropy <bradgarropy@gmail.com>
2024-04-04 10:07:47 -0500

    import theme. (#425)

--- src/scripts/copyTheme.ts
+++ src/scripts/copyTheme.ts
@@ -4,5 +4,5 @@ mkdirSync("build/client/shiki/themes", {recursive: true})
 
 copyFileSync(
     "node_modules/shades-of-purple/themes/shades-of-purple-color-theme.json",
-    "build/client/shiki/themes/shades-of-purple.json",
+    "node_modules/shades-of-purple/themes/shades-of-purple-color-theme.json5",
 )
