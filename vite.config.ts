52257fb41e021c3566100aa180b1dc828e807ce9
Brad Garropy <bradgarropy@gmail.com>
2024-04-04 10:15:34 -0500

    ⚡️ vite config (#426)
    * uninstall json5.
    
    * combine vite configs.
    
    * bump version.

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import {vitePlugin as remix} from "@remix-run/dev"
 import {installGlobals} from "@remix-run/node"
 import {vercelPreset} from "@vercel/remix/vite"
+import react from "@vitejs/plugin-react"
 import {defineConfig} from "vite"
 import json5 from "vite-plugin-json5"
 import {Mode, plugin as markdown} from "vite-plugin-markdown"
@@ -13,16 +14,34 @@ export default defineConfig({
         tsconfigPaths(),
         json5(),
         markdown({mode: [Mode.MARKDOWN]}),
-        remix({
-            appDirectory: "src",
-            ignoredRouteFiles: ["**/.*"],
-            future: {},
-            presets: [vercelPreset()],
-            serverModuleFormat: "esm",
-        }),
+        process.env.VITEST
+            ? react()
+            : remix({
+                  appDirectory: "src",
+                  ignoredRouteFiles: ["**/.*"],
+                  future: {},
+                  presets: [vercelPreset()],
+                  serverModuleFormat: "esm",
+              }),
     ],
     server: {
         open: true,
         port: 3000,
+    },
+    test: {
+        clearMocks: true,
+        coverage: {
+            all: false,
+            clean: true,
+            cleanOnRerun: true,
+            enabled: true,
+            provider: "v8",
+            reporter: ["text", "lcov"],
+            reportOnFailure: false,
+        },
+        environment: "jsdom",
+        globals: false,
+        passWithNoTests: true,
+        setupFiles: ["src/test-utils/setup.tsx"],
     },
 })
