1261c58b091267ef0d7ddcfae2914e4bf5406a03
Brad Garropy <bradgarropy@gmail.com>
2024-04-25 21:05:03 -0500

    change donate link.

--- src/routes/donate.ts
+++ src/routes/donate.ts
@@ -1,5 +1,5 @@
 import {redirect} from "@remix-run/node"
 
 export const loader = () => {
-    throw redirect("https://paypal.me/bradgarropy")
+    throw redirect("https://buy.stripe.com/5kA7upbNW9wW27CeUU")
 }
