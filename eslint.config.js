920d93c1dd4874f67bbf1608f05877b0376a6893
Brad Garropy <bradgarropy@gmail.com>
2024-10-22 12:26:17 -0500

    configure eslint.

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,37 @@
-{
-    "extends": [
-        "@remix-run/eslint-config",
-        "@remix-run/eslint-config/node",
-        "@remix-run/eslint-config/jest-testing-library",
-        "@bradgarropy/eslint-config",
-        "@bradgarropy/eslint-config-react",
-        "@bradgarropy/eslint-config-typescript"
-    ],
-    "rules": {
-        // https://github.com/testing-library/eslint-plugin-testing-library/blob/main/docs/rules/no-await-sync-events.md#when-not-to-use-it
-        "testing-library/no-await-sync-events": [
-            "error",
-            {
-                "eventModules": ["fire-event"]
-            }
-        ]
-    }
-}
+import bgConfig from "@bradgarropy/eslint-config"
+import bgReactConfig from "@bradgarropy/eslint-config-react"
+import bgTypescriptConfig from "@bradgarropy/eslint-config-typescript"
+
+const config = [
+    ...bgConfig,
+    ...bgReactConfig,
+    ...bgTypescriptConfig,
+    {
+        ignores: [
+            // dependencies
+            "/node_modules",
+            // build
+            "/.cache",
+            "/build",
+            "/public/build",
+            "/tsconfig.tsbuildinfo",
+            // test
+            "/coverage",
+            // host
+            "/.vercel",
+            // secrets
+            "/.env*",
+            // logs
+            "/debug.log",
+            // os
+            ".DS_Store",
+            // generated
+            "/public/feed.json",
+            "/public/feed.xml",
+            "/public/robots.txt",
+            "/public/sitemap.txt",
+        ],
+    },
+]
+
+export default config
