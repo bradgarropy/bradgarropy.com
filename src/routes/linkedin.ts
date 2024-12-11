733b7b22affc982c3986b3a51a990baff62127b4
Brad Garropy <bradgarropy@gmail.com>
2024-12-11 10:46:52 -0600

    remove www.

--- src/routes/linkedin.ts
+++ src/routes/linkedin.ts
@@ -1,5 +1,5 @@
 import {redirect} from "@remix-run/node"
 
 export const loader = () => {
-    throw redirect("https://www.linkedin.com/in/bradgarropy")
+    throw redirect("https://linkedin.com/in/bradgarropy")
 }
