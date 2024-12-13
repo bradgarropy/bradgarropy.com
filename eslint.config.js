6c2ac137c1c2d661c74ba2b6c01d660a727446d1
Brad Garropy <bradgarropy@gmail.com>
2024-12-12 21:21:40 -0600

    integrate josh's comments.

--- eslint.config.js
+++ eslint.config.js
@@ -7,16 +7,13 @@ import jsxA11y from "eslint-plugin-jsx-a11y"
 import react from "eslint-plugin-react"
 import reactHooks from "eslint-plugin-react-hooks"
 import simpleImportSort from "eslint-plugin-simple-import-sort"
-import globals from "globals"
 import ts from "typescript-eslint"
 
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
 const gitignore = path.resolve(dirname, ".gitignore")
 
-/** @type {import('eslint').Linter.Config[]} */
-
-const config = [
+const config = ts.config([
     includeIgnoreFile(gitignore),
     {
         files: [
@@ -29,16 +26,7 @@ const config = [
             "**/*.mts",
             "**/*.tsx",
         ],
-
         languageOptions: {
-            ecmaVersion: "latest",
-            globals: {
-                ...globals.browser,
-                ...globals.node,
-                ...globals.es2025,
-                ...globals.jest,
-            },
-            parser: ts.parser,
             parserOptions: {
                 projectService: true,
                 tsconfigRootDir: import.meta.dirname,
@@ -55,7 +43,7 @@ const config = [
         },
     },
     js.configs.recommended,
-    ...ts.configs.recommended,
+    ts.configs.recommended,
     react.configs.flat.recommended,
     react.configs.flat["jsx-runtime"],
     jsxA11y.flatConfigs.recommended,
@@ -70,13 +58,8 @@ const config = [
                     fixStyle: "separate-type-imports",
                 },
             ],
-            "@typescript-eslint/consistent-type-exports": [
-                "error",
-                {
-                    fixMixedExportsWithInlineTypeSpecifier: false,
-                },
-            ],
-            "@typescript-eslint/no-import-type-side-effects": "error",
+            "@typescript-eslint/consistent-type-exports": ["error"],
+            "@typescript-eslint/no-import-type-side-effects": ["error"],
             "@typescript-eslint/no-unused-vars": [
                 "error",
                 {
@@ -88,20 +71,13 @@ const config = [
             "indent": ["off"],
             "jsx-a11y/accessible-emoji": ["off"],
             "no-console": ["off"],
-            "no-unused-vars": [
-                "error",
-                {
-                    ignoreRestSiblings: true,
-                },
-            ],
             "prefer-const": ["error"],
-            "react/react-in-jsx-scope": ["off"],
             "react-hooks/rules-of-hooks": ["error"],
             "react-hooks/exhaustive-deps": ["error"],
             "simple-import-sort/imports": ["error"],
             "simple-import-sort/exports": ["error"],
         },
     },
-]
+])
 
 export default config
