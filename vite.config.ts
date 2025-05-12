f649f7c66b2bb7c21d5c88e842750da27411018c
Brad Garropy <bradgarropy@gmail.com>
2025-05-12 11:19:41 -0500

    switch to react router devtools.

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,11 @@
-import {vitePlugin as remix} from "@remix-run/dev"
+import {reactRouter} from "@react-router/dev/vite"
 import tailwind from "@tailwindcss/vite"
-import {vercelPreset} from "@vercel/remix/vite"
 import react from "@vitejs/plugin-react"
-import {remixDevTools} from "remix-development-tools"
+import {reactRouterDevTools} from "react-router-devtools"
 import {defineConfig} from "vite"
 import json5 from "vite-plugin-json5"
 import {Mode, plugin as markdown} from "vite-plugin-markdown"
 import tsconfigPaths from "vite-tsconfig-paths"
-
-declare module "@remix-run/node" {
-    interface Future {
-        v3_singleFetch: true
-    }
-}
 
 export default defineConfig({
     plugins: [
@@ -20,28 +13,12 @@ export default defineConfig({
         tailwind(),
         json5(),
         markdown({mode: [Mode.MARKDOWN]}),
-        remixDevTools({
+        reactRouterDevTools({
             client: {
                 showBreakpointIndicator: false,
             },
         }),
-        process.env.VITEST
-            ? react()
-            : remix({
-                  appDirectory: "src",
-                  ignoredRouteFiles: ["**/.*"],
-                  future: {
-                      unstable_optimizeDeps: true,
-                      v3_fetcherPersist: true,
-                      v3_relativeSplatPath: true,
-                      v3_throwAbortReason: true,
-                      v3_lazyRouteDiscovery: true,
-                      v3_singleFetch: true,
-                      v3_routeConfig: true,
-                  },
-                  presets: [vercelPreset()],
-                  serverModuleFormat: "esm",
-              }),
+        process.env.VITEST ? react() : reactRouter(),
     ],
     server: {
         open: true,
