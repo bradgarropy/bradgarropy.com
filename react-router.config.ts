280207c2ba10220d6f37101ac94beea46654d40c
Brad Garropy <bradgarropy@gmail.com>
2025-05-12 11:12:34 -0500

    add react router config.

---
+++ react-router.config.ts
@@ -0,0 +1,14 @@
+import type {Config} from "@react-router/dev/config"
+import {vercelPreset} from "@vercel/remix/vite"
+
+const config = {
+    appDirectory: "src",
+    future: {
+        unstable_optimizeDeps: true,
+    },
+    presets: [vercelPreset()],
+    serverModuleFormat: "esm",
+    ssr: true,
+} satisfies Config
+
+export default config
