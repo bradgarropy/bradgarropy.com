633649d743aedc300bcf934761cbc8192fa7d5e1
Brad Garropy <bradgarropy@gmail.com>
2026-07-22 22:44:57 -0500

    improve link accessibility.

--- src/components/Markdown/Markdown.tsx
+++ src/components/Markdown/Markdown.tsx
@@ -10,7 +10,7 @@ const Markdown: FC<MarkdownProps> = ({content, className}) => {
     return (
         <div
             className={classnames(
-                "prose-md prose-[iframe]:w-100 prose prose-purple dark:prose-invert prose-a:font-normal prose-a:text-gray-700 prose-a:no-underline prose-a:shadow-link prose-a:transition prose-a:duration-300 prose-a:hover:shadow-link-hover prose-code:rounded prose-code:bg-[#2d2b55] prose-code:px-2 prose-code:py-1 prose-code:font-normal prose-code:text-[#fad000] prose-code:before:content-none prose-code:after:content-none prose-pre:bg-[#2D2B55] prose-img:mx-auto prose-img:rounded dark:prose-a:text-gray-300 max-w-3xl",
+                "prose-md prose-[iframe]:w-100 prose prose-purple dark:prose-invert prose-a:font-normal prose-a:text-gray-700 prose-a:underline prose-a:decoration-purple-400 prose-a:decoration-2 prose-a:underline-offset-4 prose-a:transition-colors prose-a:duration-300 prose-a:hover:decoration-purple-600 dark:prose-a:text-gray-300 dark:prose-a:hover:decoration-purple-300 prose-code:rounded prose-code:bg-[#2d2b55] prose-code:px-2 prose-code:py-1 prose-code:font-normal prose-code:text-[#fad000] prose-code:before:content-none prose-code:after:content-none prose-pre:bg-[#2D2B55] prose-img:mx-auto prose-img:rounded max-w-3xl",
                 className,
             )}
         >
