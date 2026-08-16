5b63bbfa46caff4f5328d9d1ee9f02286ab137dc
Brad Garropy <bradgarropy@gmail.com>
2026-08-15 22:40:22 -0500

    consume file globs.

--- src/utils/testimonials.ts
+++ src/utils/testimonials.ts
@@ -1,14 +1,10 @@
 import {testimonialFrontmatterSchema} from "~/schemas/testimonial"
-import type {Markdown} from "~/types/markdown"
 import type {Testimonial} from "~/types/testimonial"
+import {testimonialFiles} from "~/utils/files.server"
 import {transformMarkdown} from "~/utils/markdown.server"
 
 const getTestimonials = async (): Promise<Testimonial[]> => {
-    const files = import.meta.glob<Markdown>("/content/testimonials/*.md", {
-        eager: true,
-    })
-
-    const promises = Object.values(files).map(async file => {
+    const promises = Object.values(testimonialFiles).map(async file => {
         const html = await transformMarkdown(file.markdown)
         const frontmatter = testimonialFrontmatterSchema.parse(file.attributes)
 
